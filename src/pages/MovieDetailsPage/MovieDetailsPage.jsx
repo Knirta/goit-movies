971005aa5807ce3407c9bd5212b3a6a90b9bc9c9
Knirta <myknirtak@gmail.com>
2024-11-15 12:09:13 +0200

    add movieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,46 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Container from "../../components/Container";
+import { MdKeyboardBackspace } from "react-icons/md";
+import movies_API from "../../sevices/movies-api";
+import s from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const movie = await movies_API.fetchMovieById(movieId);
+      setMovie(movie);
+    };
+    getMovie();
+  }, [movieId]);
+
   return (
     <Container>
-      <h1>Movie {movieId} details page</h1>
+      <section className={s.section}>
+        <button className={s.btn} role="button">
+          <MdKeyboardBackspace /> Go back
+        </button>
+        {movie && (
+          <div className={s.wrap}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}.jpg`}
+              width="400"
+              alt={movie.title}
+            />
+            <div className={s.info}>
+              <h1 className={s.title}>{movie.title}</h1>
+              <p>Popularity: {movie.popularity}</p>
+              <h2>Overview</h2>
+              <p>{movie.overview}</p>
+              <h3>Genres</h3>
+              <p>{movie.genres.map(({ name }) => name).join(", ")}</p>
+            </div>
+          </div>
+        )}
+      </section>
     </Container>
   );
 };
--- src/sevices/movies-api.js
+++ src/sevices/movies-api.js
@@ -5,11 +5,16 @@ axios.defaults.headers.common["Authorization"] =
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGJlNWY4Y2UzZTExZjkwMjBjNmI1ZDE0M2ZlMzc4ZSIsIm5iZiI6MTczMTQwMzA1NC4yNjMwMDIyLCJzdWIiOiI2NzMyNjUxMzU3NWQwNjlkMzlmYzZiM2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.6Y1FuKg672DRYw95zWOoHb4hRPUNj-vtYOVVCoYK5IY";
 axios.defaults.headers.common["Accept"] = "application/json";
 
-let fetchTrendingMovies = async () => {
+const fetchTrendingMovies = async () => {
   let { data: { results } = [] } = await axios({ url: "/trending/movie/day" });
   return results;
 };
 
-const api = { fetchTrendingMovies };
+const fetchMovieById = async (movieId) => {
+  let { data } = await axios({ url: `/movie/${movieId}` });
+  return data;
+};
+
+const api = { fetchTrendingMovies, fetchMovieById };
 
 export default api;
